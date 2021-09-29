c9aaff6f4c4c88747316dab62c970c3d238eeb40
Mateus <mat3usrc@gmail.com>
2021-09-29 10:04:07 -0300

    Update test and dates

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -3,12 +3,14 @@ import React from 'react';
 import styles from './Meta.module.scss';
 
 type Props = {
-  date: string
+  date: string,
 };
 
 const Meta = ({ date }: Props) => (
   <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</p>
+    <p className={styles['meta__date']}>
+      Publicado em {new Date(date).toLocaleDateString('pt-BR', { year: 'numeric', month: 'short', day: 'numeric' })}
+    </p>
   </div>
 );
 
